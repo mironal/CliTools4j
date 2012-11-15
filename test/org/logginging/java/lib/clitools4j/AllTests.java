e93e3ad82f4dd058b5430de3b05a5022fbb8564f
mironal <miro.gunp@gmail.com>
2012-11-15 21:41:03 +0900

    add TestSuite

---
+++ test/org/logginging/java/lib/clitools4j/AllTests.java
@@ -0,0 +1,11 @@
+package org.logginging.java.lib.clitools4j;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({OptionsTest.class, CommandTest.class, CommandLineExecutorTest.class})
+public class AllTests {
+
+}
